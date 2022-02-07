e18d532f6d560e0697a7524f668638887bd22361
Ndiaye <diabir_n@etna-alternance.net>
2022-02-07 16:07:50 +0100

    Initial structure with static books list

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TitleH1 from './components/Titles/TitleH1/TitleH1';
+
+function App() {
+  return (
+    <div>
+      <TitleH1>Ma liste de livres</TitleH1>
+    </div>
+  );
+}
+
+export default App;
